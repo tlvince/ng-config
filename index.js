589a465bcc3442082b10344b520fabbe681ed76e
Tom Vincent <git@tlvince.com>
2014-12-12 15:51:36 +0000

    Full path to template

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 'use strict';
 
 var fs = require('fs');
+var path = require('path');
 var extend = require('extend');
 var microtemplates = require('microtemplates');
 
 var defaults = {
   module: 'config',
-  template: './index.tpl',
+  template: path.join(__dirname, './index.tpl'),
   constants: {}
 };
 
